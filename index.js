18819d5211e5af450ace9dfacd8bb6e9a48b3e9b
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-05-03 22:17:14 -0500

    start writing fetchJobs function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi");
+function fetchJobs() {
+  fetch("http://localhost:3000/jobs")
+    .then((res) => res.json())
+    .then((jobs) => {
+      console.log(jobs);
+    });
+}
+fetchJobs();
